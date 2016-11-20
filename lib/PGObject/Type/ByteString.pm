67d88216237f6a0cef6ce4006cf984cd20830a96
Erik Huelsmann <ehuels@gmail.com>
2016-11-20 12:57:09 +0100

    * Update version

--- lib/PGObject/Type/ByteString.pm
+++ lib/PGObject/Type/ByteString.pm
@@ -13,11 +13,11 @@ PGObject::Type::DateTime - DateTime Wrappers for PGObject
 
 =head1 VERSION
 
-Version 1.0.0
+Version 1.1.0
 
 =cut
 
-our $VERSION = '1.0.0';
+our $VERSION = '1.1.0';
 
 
 =head1 SYNOPSIS
